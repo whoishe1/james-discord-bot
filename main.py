ec4084f39f1c6133471b86f3a2adafb85c3efb08
whoishe1 <jordanko9112@gmail.com>
2021-05-15 17:57:02 -0700

    set owner_id

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import os
 
 #Settings
 intents = discord.Intents.all()
-client = commands.Bot(command_prefix="!", intents=intents)
+client = commands.Bot(command_prefix="!", owner_id = 249819836668968962, intents=intents)
 client.remove_command("help")
 
 #Find and load cogs
